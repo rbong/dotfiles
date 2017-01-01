8914df4ad89d80df488d61a42e2f18a847fa2ba4
rbong <roger.bongers@yahoo.ca>
2017-01-01 10:55:50 -0500

    Now making vim dir.

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-echo setting up dependencies...
-echo installing plugged...
-# https://github.com/junegunn/vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo installing vim plugins...
-nvim -c "PlugInstall | qa"
-echo done setting up dependencies.
-
 echo moving dotfiles...
 cp .zshrc .vimrc .pentadactylrc ~
 mkdir -p ~/.vifm
@@ -15,8 +7,17 @@ echo done moving dotfiles.
 
 echo linking vim to neovim...
 mkdir -p ~/.config/nvim
+mkdir -p ~/.vim
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 echo done linking.
+
+echo setting up dependencies...
+echo installing plugged...
+# https://github.com/junegunn/vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo installing vim plugins...
+nvim -c "PlugInstall | qa"
+echo done setting up dependencies.
 
 echo done all.
