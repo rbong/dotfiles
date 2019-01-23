cfbc1d9e519ecab7c3e1680bd7f1ceccce19f0cb
rbong <r.l.bongers@gmail.com>
2019-01-23 15:16:06 -0500

    Force copying dotfiles
    Ignores missing dotfiles.

--- pull_updates.sh
+++ pull_updates.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo pulling dotfiles
-cp ~/.zshrc ~/.vimrc ~/.Xresources ~/.xinitrc ~/.conkyrc ~/.cvimrc .
+cp -f ~/.zshrc ~/.vimrc ~/.Xresources ~/.xinitrc ~/.conkyrc ~/.cvimrc .
 
 if [[ -f ~/.vifm/vifmrc ]]; then
   echo pulling vfmrc
