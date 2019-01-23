86993a295ef970a8edeb3891e5036bea9892b18d
rbong <r.l.bongers@gmail.com>
2019-01-23 12:48:39 -0500

    Add pull_updates.sh script

---
+++ pull_updates.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo pulling dotfiles
+cp ~/.zshrc ~/.vimrc ~/.Xresources ~/.xinitrc ~/.conkyrc .
+
+if [[ -f ~/.vifm/vifmrc ]]; then
+  echo pulling vfmrc
+  cp ~/.vifm/vifmrc .
+else
+  echo skipping ~/.vifm/vifmrc
+fi
+
+if [[ -d ~/.vim/UltiSnips ]]; then
+  echo pulling UltiSnips
+  cp -R ~/.vim/UltiSnips .
+else
+  echo skipping ~/.vim/UltiSnips
+fi
